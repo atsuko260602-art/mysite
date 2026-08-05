5b3f1a106809cdebedceaad4ecddd1d1bc5c9a63
atsuko260602-art <atsuko260602@gmail.com>
2026-08-05 10:36:27 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll",()=>{
+
+    sections.forEach(section=>{
+
+        const position =
+        section.getBoundingClientRect().top;
+
+        if(position < window.innerHeight - 100){
+
+            section.classList.add("show");
+
+        }
+
+    });
+
+});
